d45b2fe71c0aed238b0156877d55ee89906d814e
MichaelM234 <149825145+MichaelM234@users.noreply.github.com>
2025-08-29 21:37:52 +0000

    Final FINAL iteration

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-// --- linkable wrappers for inline-only SDK functions ---
+// wrappers
 void gpio_set_dir_wrapper(uint gpio, bool out) { gpio_set_dir(gpio, out); }
 void gpio_put_wrapper(uint gpio, bool value)   { gpio_put(gpio, value);  }
 
-// main assembly entry point
+// assembly entry point
 void main_asm(void);
 
 int main(void) {
